dc9bc9c7378f647cdf017af1e7c1f1a44e8e512d
Mirror-Prismals <PurpleRed80@proton.me>
2025-09-07 07:14:55 -0400

    Create BaseSystem.cpp

---
+++ 5/BaseSystem.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// The "Base System" data object. Holds all data shared between systems.
+struct BaseSystem {
+    float cameraYaw = -90.0f;
+    float cameraPitch = 0.0f;
+    glm::vec3 cameraPosition = glm::vec3(6.0f, 5.0f, 15.0f);
+    float mouseOffsetX = 0.0f;
+    float mouseOffsetY = 0.0f;
+    float lastX = WINDOW_WIDTH / 2.0f;
+    float lastY = WINDOW_HEIGHT / 2.0f;
+    bool firstMouse = true;
+    glm::mat4 viewMatrix;
+    glm::mat4 projectionMatrix;
+};
