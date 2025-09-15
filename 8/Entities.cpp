84f0102c2088462752ffd2c9624ae3ad9833c5f0
Mirror-Prismals <PurpleRed80@proton.me>
2025-09-15 16:51:21 -0400

    Create Entities.cpp

---
+++ 8/Entities.cpp
@@ -0,0 +1,36 @@
+#pragma once
+
+// Factory function to create all the entity prototypes
+std::vector<Entity> createAllEntityPrototypes(BaseSystem& baseSystem) {
+    std::vector<Entity> entityPrototypes;
+
+    // Use the numBlockPrototypes value loaded from JSON
+    for (int i = 0; i < baseSystem.numBlockPrototypes; ++i) {
+        Entity p;
+        p.prototypeID = entityPrototypes.size();
+        p.isRenderable = true;
+        p.blockType = i;
+        p.name = "BlockType_" + std::to_string(i);
+        entityPrototypes.push_back(p);
+    }
+    
+    Entity worldProto;
+    worldProto.prototypeID = entityPrototypes.size();
+    worldProto.name = "World";
+    worldProto.isWorld = true;
+    entityPrototypes.push_back(worldProto);
+    
+    Entity starProto;
+    starProto.prototypeID = entityPrototypes.size();
+    starProto.name = "Star";
+    starProto.isStar = true;
+    entityPrototypes.push_back(starProto);
+
+    Entity worldGenAudicle;
+    worldGenAudicle.prototypeID = entityPrototypes.size();
+    worldGenAudicle.name = "DebugWorldGenerator";
+    worldGenAudicle.isAudicle = true;
+    entityPrototypes.push_back(worldGenAudicle);
+
+    return entityPrototypes;
+}
