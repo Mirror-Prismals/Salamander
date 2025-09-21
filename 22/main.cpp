be01f07f4fa98abfa924901e91c332960fc7a1fa
Mirror-Prismals <PurpleRed80@proton.me>
2025-09-20 21:12:41 -0400

    Create main.cpp

---
+++ 22/main.cpp
@@ -0,0 +1,28 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "Host.h"
+
+// --- Include System Implementations for Single Translation Unit Build ---
+#include "BaseEntity.cpp"
+#include "BaseSystem/SkyboxSystem.cpp" // <-- ADD THIS LINE
+#include "BaseSystem/KeyboardInputSystem.cpp"
+#include "BaseSystem/MouseInputSystem.cpp"
+#include "BaseSystem/UAVSystem.cpp"
+#include "BaseSystem/TesseractSystem.cpp"
+#include "BaseSystem/AudioSystem.cpp"
+#include "BaseSystem/RayTracedAudioSystem.cpp"
+#include "BaseSystem/PinkNoiseSystem.cpp"
+#include "BaseSystem/AudicleSystem.cpp"
+#include "BaseSystem/CameraSystem.cpp"
+#include "BaseSystem/RenderSystem.cpp"
+#include "Host/HostShader.cpp"
+#include "Host/HostUtilities.cpp"
+#include "Host/Startup.cpp"
+#include "Host/HostInput.cpp"
+#include "Host/HostLoader.cpp"
+#include "Host/Host.cpp"
+
+int main() {
+    Host cardinal;
+    cardinal.run();
+    return 0;
+}
