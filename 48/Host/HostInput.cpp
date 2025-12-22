e223ec243bea2f9f48729e60fca723146e3d0261
Mirror-Prismals <PurpleRed80@proton.me>
2025-12-22 02:25:34 -0500

    Add files via upload

---
+++ 48/Host/HostInput.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+void Host::processMouseInput(double xpos, double ypos) {
+    if (baseSystem.player) {
+        PlayerContext& player = *baseSystem.player;
+        if (player.firstMouse) { player.lastX = xpos; player.lastY = ypos; player.firstMouse = false; }
+        player.mouseOffsetX = xpos - player.lastX;
+        player.mouseOffsetY = player.lastY - ypos;
+        player.lastX = xpos;
+        player.lastY = ypos;
+    }
+}
+
+void Host::processScroll(double xoffset, double yoffset) {
+    if (baseSystem.player) {
+        PlayerContext& player = *baseSystem.player;
+        player.scrollYOffset += yoffset;
+    }
+}
---
+++ 48/main.cpp
@@ -0,0 +1,48 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "Host.h"
+
+// --- Include System Implementations for Single Translation Unit Build ---
+#include "BaseEntity.cpp"
+#include "BaseSystem/SpawnSystem.cpp"
+#include "BaseSystem/CollisionSystem.cpp"
+#include "BaseSystem/WalkModeSystem.cpp"
+#include "BaseSystem/GravitySystem.cpp"
+#include "BaseSystem/SkyboxSystem.cpp" // <-- ADD THIS LINE
+#include "BaseSystem/KeyboardInputSystem.cpp"
+#include "BaseSystem/MouseInputSystem.cpp"
+#include "BaseSystem/UAVSystem.cpp"
+#include "BaseSystem/AudioSystem.cpp"
+#include "BaseSystem/RayTracedAudioSystem.cpp"
+#include "BaseSystem/AudioRayVisualizerSystem.cpp"
+#include "BaseSystem/PinkNoiseSystem.cpp"
+#include "BaseSystem/AudicleSystem.cpp"
+#include "BaseSystem/CameraSystem.cpp"
+#include "BaseSystem/ChunkSystem.cpp"
+#include "BaseSystem/RenderSystem.cpp"
+#include "BaseSystem/CloudSystem.cpp"
+#include "BaseSystem/AuroraSystem.cpp"
+#include "BaseSystem/VolumeFillSystem.cpp"
+#include "BaseSystem/GlyphSystem.cpp"
+#include "BaseSystem/BlockSelectionSystem.cpp"
+#include "BaseSystem/BlockChargeSystem.cpp"
+#include "BaseSystem/BuildSystem.cpp"
+#include "BaseSystem/StructurePlacementSystem.cpp"
+#include "BaseSystem/StructureCaptureSystem.cpp"
+#include "BaseSystem/HUDSystem.cpp"
+#include "BaseSystem/TerrainGenerationSystem.cpp"
+#include "BaseSystem/UIScreenSystem.cpp"
+#include "BaseSystem/ComputerCursorSystem.cpp"
+#include "BaseSystem/ButtonSystem.cpp"
+#include "BaseSystem/ChucKSystem.cpp"
+#include "Host/HostShader.cpp"
+#include "Host/HostUtilities.cpp"
+#include "Host/Startup.cpp"
+#include "Host/HostInput.cpp"
+#include "Host/HostLoader.cpp"
+#include "Host/Host.cpp"
+
+int main() {
+    Host cardinal;
+    cardinal.run();
+    return 0;
+}
